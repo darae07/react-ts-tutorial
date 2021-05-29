432b3d9e2f54653a55c907dc813d18bf5293da68
darae07 <leedr0730@gmail.com>
2021-05-30 01:46:53 +0900

    차트 생성

--- components/main/chart.tsx
+++ components/main/chart.tsx
@@ -1,6 +1,11 @@
-import styles from './chart.module.css'
+import { useEffect } from 'react';
+import styles from './chart.module.css';
+import {getBillboardChart} from 'controllers/chart';
 
 export function Chart(){
+  useEffect(()=>{
+    // getBillboardChart()
+  }, [])
   return (
     <div className={styles.chart}>
       <h2>멜론 차트</h2>
---
+++ controllers/chart.tsx
@@ -0,0 +1,64 @@
+import axios from "axios";
+
+const formatter = (int) => {
+  return int > 9 ? int : '0'+int;
+}
+const today = () => {
+  const d = new Date();
+  const y = d.getFullYear();
+  const m = d.getMonth()+1;
+  const dd = d.getDate();
+  return `${y}-${formatter(m)}-${formatter(dd)}`
+}
+const options = {
+  method: 'GET',
+  url: 'https://billboard-api2.p.rapidapi.com/hot-100',
+  params: {date: today(), range: '1-10'},
+  headers: {
+    'x-rapidapi-key': '1d216a6414msh210933d608d4962p1bc50bjsnb8622dbadd16',
+    'x-rapidapi-host': 'billboard-api2.p.rapidapi.com'
+  }
+};
+
+export function getBillboardChart(){
+  axios.request(options).then(function (response) {
+    console.log(response.data);
+  }).catch(function (error) {
+    console.error(error);
+  });
+}
+
+export const data = {
+  content: [
+    {
+      artist: "Olivia Rodrigo",
+      detail: "new",
+      'last week': "None",
+      'peak position': "1",
+      rank: "1",
+      title: "Good 4 U",
+      'weeks at no.1': "1",
+      'weeks on chart': "1",
+    },
+    {
+      artist: "Olivia Rodrigo",
+      detail: "new",
+      'last week': "None",
+      'peak position': "1",
+      rank: "1",
+      title: "Good 4 U",
+      'weeks at no.1': "1",
+      'weeks on chart': "1",
+    },
+    {
+      artist: "Olivia Rodrigo",
+      detail: "new",
+      'last week': "None",
+      'peak position': "1",
+      rank: "1",
+      title: "Good 4 U",
+      'weeks at no.1': "1",
+      'weeks on chart': "1",
+    }
+  ]
+}
